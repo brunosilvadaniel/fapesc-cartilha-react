1e979a1c80e7c6ea6b871230feb318c4c19af607
Bruno Silva Daniel <bruno_silva_daniel@hotmail.com>
2020-05-22 03:30:46 -0300

    Add NavBarJogos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import NavBar from './components/Navbar/index'
+import NavBarJogos from './components/NavBarJogos/index'
 import Footer from './components/Footer/index'
+
 
 function App() {
   return (
     <div >
       <NavBar/>
+      <NavBarJogos/>
       <Footer/>
     </div>
   );
---
+++ src/components/NavBarJogos/index.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+import './index.css';
+
+export default class NavBarJogos extends Component{
+  render(){
+    return(
+      <div className="NavBarJogos">
+        <h1 className="titulo">Jogos</h1>
+        <div className="Jogos">
+          <div className="Jogo jogo-fundo-laranja-escuro">
+            <h1 className="jogo-titulo ">Jogo da Memoria</h1>
+          </div>
+          <div className="Jogo jogo-fundo-vermelho">
+            <h1 className="jogo-titulo ">Quebra Cabeça</h1>
+          </div>
+          <div className="Jogo jogo-fundo-laranja-claro">
+           <h1 className="jogo-titulo">Caça Palavras</h1>
+          </div>
+          <div className="Jogo jogo-fundo-verde">
+          <h1 className="jogo-titulo ">Quarto jogo</h1>
+          </div>          
+        </div>
+
+      </div>
+
+    )
+  }
+}
